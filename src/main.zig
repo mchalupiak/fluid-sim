7ece3b768d503127dfdf1ea07372b93a89c7d33e
Michael Chalupiak <chalupmc@rose-hulman.edu>
2023-10-10 14:45:51 -0400

    refactored with casting functions, cleaner to read

--- src/main.zig
+++ src/main.zig
@@ -8,7 +8,7 @@ const Height: i32 = @divTrunc(Width * 3, 4);
 const HalfWidth: f32 = Width / 2;
 const HalfHeight: f32 = Height / 2;
 
-const uiScale: f32 = (@as(f32, @floatFromInt(Width)) / 2400.0);
+const uiScale: f32 = float(Width) / 2400.0;
 const uiWidth = 315 * uiScale;
 const uiHeight = 60 * uiScale;
 const uiBoxSize = 40 * uiScale;
@@ -36,14 +36,14 @@ const Particle = struct {
 };
 
 fn init(particles: *std.ArrayList(Particle)) void {
-    const particlesPerRow: u32 = @intFromFloat(math.sqrt(numParticles));
-    const particlesPerCol: u32 = (@as(u32, @intFromFloat(numParticles)) - 1) / particlesPerRow + 1;
+    const particlesPerRow: u32 = uint(math.sqrt(numParticles));
+    const particlesPerCol: u32 = (uint(numParticles) - 1) / particlesPerRow + 1;
     const spacing: f32 = particleSize * 2 + particleSpacing;
     particles.clearRetainingCapacity();
 
-    for (0..@as(u32, @intFromFloat(numParticles))) |i| {
-        const x: f32 = (@as(f32, @floatFromInt(i % particlesPerRow)) - @as(f32, @floatFromInt(particlesPerRow)) / 2.0 + 0.5) * spacing;
-        const y: f32 = (@as(f32, @floatFromInt(i / particlesPerRow)) - @as(f32, @floatFromInt(particlesPerCol)) / 2.0 + 0.5) * spacing;
+    for (0..uint(numParticles)) |i| {
+        const x: f32 = (float(i % particlesPerRow) - float(particlesPerRow) / 2.0 + 0.5) * spacing;
+        const y: f32 = (float(i / particlesPerRow) - float(particlesPerCol) / 2.0 + 0.5) * spacing;
         particles.append(Particle.init(x + HalfWidth, y + HalfHeight)) catch unreachable;
     }
 }
@@ -60,7 +60,7 @@ fn update(width: f32, height: f32, particles: *std.ArrayList(Particle), run: *bo
         rl.DrawCircleV(particles.items[i].position, particleSize, rl.BLUE);
     }
     if (reset.*) {
-        particles.resize(@as(usize, @intFromFloat(numParticles))) catch unreachable;
+        particles.resize(uint(numParticles)) catch unreachable;
         init(particles);
         run.* = false;
     }
@@ -95,7 +95,7 @@ pub fn main() !void {
 
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
-    var particles = std.ArrayList(Particle).initCapacity(allocator, @as(usize, @intFromFloat(@floor(numParticles)))) catch unreachable;
+    var particles = std.ArrayList(Particle).initCapacity(allocator, uint(numParticles)) catch unreachable;
     init(&particles);
     defer particles.deinit();
 
@@ -109,7 +109,7 @@ pub fn main() !void {
         defer rl.EndDrawing();
         rl.ClearBackground(rl.RAYWHITE);
         rl.DrawFPS(10, 10);
-        rl.DrawRectangleLines(@as(i32, @intFromFloat(HalfWidth - colBoxX / 2)), @as(i32, @intFromFloat(HalfHeight - colBoxY / 2)), @as(i32, @intFromFloat(colBoxX)), @as(i32, @intFromFloat(colBoxY)), rl.BLACK);
+        rl.DrawRectangleLines(int(HalfWidth - colBoxX / 2), int(HalfHeight - colBoxY / 2), int(colBoxX), int(colBoxY), rl.BLACK);
         update(colBoxX + HalfWidth - colBoxX / 2, colBoxY + HalfHeight - colBoxY / 2, &particles, &run, &reset);
         drawUI(&colBoxX, &colBoxY, &run, &reset);
         const msg = "Hello zig! You created your first window.";
@@ -130,4 +130,16 @@ fn drawUI(colBoxX: *f32, colBoxY: *f32, run: *bool, reset: *bool) void {
     _ = rg.GuiSliderBar(.{ .x = xCol, .y = 70 + (uiHeight * 5), .width = uiWidth, .height = uiHeight }, "Collision Dampening", "", &collisionDampener, 0, 1);
     _ = rg.GuiCheckBox(.{ .x = xCol, .y = 80 + (uiHeight * 6), .width = uiBoxSize, .height = uiBoxSize }, "Run", run);
     _ = rg.GuiCheckBox(.{ .x = xCol, .y = 80 + (uiHeight * 7), .width = uiBoxSize, .height = uiBoxSize }, "Reset", reset);
+}
+
+fn float(val: anytype) f32 {
+    return @as(f32, @floatFromInt(val));
+}
+
+fn int(val: anytype) i32 {
+    return @as(i32, @intFromFloat(val));
+}
+
+fn uint(val: anytype) u32 {
+    return @as(u32, @intFromFloat(val));
 }
